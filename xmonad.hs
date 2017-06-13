2b5a1d9a39f41c2ab9e944f7af9a1ac19f76c252
Nicolas Knoebber <knoebber@gmail.com>
2017-06-12 19:29:20 -0500

    add hotkey for firefox

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,17 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 main = do
-  xmonad $ defaultConfig
-    { terminal    = myTerminal
-    , modMask     = myModMask
-    , borderWidth = myBorderWidth
-    }
-
-myTerminal    = "alacritty"
-myModMask     = mod4Mask
-myBorderWidth = 3
+    xmonad $ defaultConfig
+        { terminal   = "alacritty"
+        , manageHook = manageDocks <+> manageHook defaultConfig -- need to find out what these 2 do
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig -- avoidStruts?
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_f), spawn "firefox") -- super shift f
+        , ((controlMask .|. shiftMask, xK_s), spawn "scrot") -- super shift s, doesn't seem to work yet
+        ]
