ba79b96075d3bb3212af0cc3e5e90e1095dcb1f1
Nicolas Knoebber <knoebber@gmail.com>
2017-07-02 17:50:40 -0500

    mess with bar more

--- xmonad.hs
+++ xmonad.hs
@@ -11,6 +11,10 @@ main = do
         { terminal   = "alacritty"
         , manageHook = manageDocks <+> manageHook defaultConfig -- need to find out what these 2 do
         , layoutHook = avoidStruts  $  layoutHook defaultConfig -- avoidStruts?
+        , logHook    = dynamicLogWithPP xmobarPP
+                           { ppOutput = hPutStrLn xmproc
+                           , ppTitle  = xmobarColor "green" "" . shorten 50
+                           }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_f), spawn "firefox") -- super shift f
