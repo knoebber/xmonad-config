087c05812082b741ead6aae17596c789945355b7
Nicolas Knoebber <knoebber@gmail.com>
2017-07-02 23:59:31 -0500

    add custom workspace names

--- xmonad.hs
+++ xmonad.hs
@@ -7,20 +7,24 @@ import System.IO
 
 
 main = do
-    xmproc <- spawnPipe "xmobar /home/nicolas/.xmonad/xmobarrc"
-    xmonad $ defaultConfig
-        { modMask            = mod4Mask
-        , terminal           = "alacritty"
-        , manageHook         = manageDocks <+> manageHook defaultConfig
-        , layoutHook         = avoidStruts  $ layoutHook defaultConfig
-        , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
-        , logHook            = dynamicLogWithPP xmobarPP
-            { ppOutput          = hPutStrLn xmproc
-            ,  ppTitle          = xmobarColor "green" "black" . shorten 50
-            , ppHiddenNoWindows = xmobarColor "gray" "black"
-            }
-        } `additionalKeys`
-        [
-         ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
-        ]
+  xmproc <- spawnPipe "xmobar /home/nicolas/.xmonad/xmobarrc"
+  xmonad $ defaultConfig
+    { modMask            = mod4Mask
+    , terminal           = "alacritty"
+    , workspaces         = myWorkspaces
+    , manageHook         = manageDocks <+> manageHook defaultConfig
+    , layoutHook         = avoidStruts  $ layoutHook defaultConfig
+    , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
+    , logHook            = dynamicLogWithPP xmobarPP
+    { ppOutput          = hPutStrLn xmproc
+      , ppTitle          = xmobarColor "green" "black" . shorten 50
+      , ppHiddenNoWindows = xmobarColor "gray" "black"
+    }
+    } `additionalKeys`
+    [
+      ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
+    ]
+
+myWorkspaces = names ++ map show [length names + 1..9]
+               where names = ["web","dev","music"]
 
